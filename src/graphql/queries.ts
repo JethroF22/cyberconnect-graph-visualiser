3430e151ec250171f732cea04ebcf6b6aa93e746
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2022-01-22 13:07:36 +0200

    Update properties returned by `following` query

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -4,16 +4,29 @@ export const following = gql`
   query identity($address: String!) {
     identity(address: $address) {
       address
+      domain
+      ens
+      social {
+        twitter
+      }
+      avatar
+      joinTime
       followingCount
       followerCount
       followers {
         list {
           address
+          domain
+          ens
+          avatar
         }
       }
       followings {
         list {
           address
+          domain
+          ens
+          avatar
         }
       }
     }
