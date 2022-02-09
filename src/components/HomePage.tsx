d3541b8493fe81a846ed678a84ac2fef54758d49
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2022-02-09 19:31:36 +0200

    Remove side panel from home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,4 @@
-import Drawer from "@mui/material/Drawer";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 
 import CustomGraph from "./CustomGraph";
 import Header from "./Header";
@@ -10,43 +7,6 @@ import WithContext from "../hocs/WithContext";
 function HomePage() {
   return (
     <Box sx={{ width: "95vw", height: "95vh", display: "flex" }}>
-      <Drawer
-        sx={{
-          width: "20%",
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: "20%",
-            boxSizing: "border-box",
-            borderRight: "2px solid rgba(0, 0, 0, 0.12)",
-            backgroundColor: "#457eac",
-            padding: "10px",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            padding: "10px",
-            width: "80%",
-            margin: "0 auto",
-          }}
-        >
-          <Typography
-            variant="h3"
-            noWrap
-            component="div"
-            textAlign="center"
-            color="#fff"
-          >
-            Faceblock
-          </Typography>
-          <PersonSearchIcon sx={{ fontSize: "48px", color: "#fff" }} />
-        </Box>
-      </Drawer>
       <Box sx={{ display: "flex", flex: 1, flexDirection: "column" }}>
         <Header />
         <CustomGraph />
